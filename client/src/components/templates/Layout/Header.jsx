695400c32c3d2c8fca7f30447ce11e7a8ccff130
Olena Olefir <olefirelena98@gmail.com>
2023-03-06 22:45:24 -0500

    layout styles fixed

--- client/src/components/templates/Layout/Header.jsx
+++ client/src/components/templates/Layout/Header.jsx
@@ -5,26 +5,29 @@ import './Header.css';
 
 export const Header = () => {
   return (
-    <div className="header-container">
-      <NavLink to="/" href="/" className="name-logo-container">
-        <img
-          src="logo.jpg"
-          alt=""
-          width="70px"
-          height="70px"
-          className="logo"
-        />
-        <h1 id="name-header">Trips&Tips</h1>
-      </NavLink>
-      <Search />
-      <div className="buttons-header-container">
-        <NavLink to="new-sight" href="new-sight">
-          <Button title="Add Sight" />
+    <>
+      <div className="header-placeholder" />
+      <div className="header-container">
+        <NavLink to="/" href="/" className="name-logo-container">
+          <img
+            src="logo.jpg"
+            alt=""
+            width="70px"
+            height="70px"
+            className="logo"
+          />
+          <h1 id="name-header">Trips&Tips</h1>
         </NavLink>
-        <NavLink to="login" href="secondary">
-          <Button title="Log in" secondary />
-        </NavLink>
+        <Search />
+        <div className="buttons-header-container">
+          <NavLink to="new-sight" href="new-sight">
+            <Button title="Add Sight" />
+          </NavLink>
+          <NavLink to="login" href="secondary">
+            <Button title="Log in" secondary />
+          </NavLink>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
