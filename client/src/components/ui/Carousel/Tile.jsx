d1beffd783e7fe690af36fdf66cc246d85b69d8b
Olena Olefir <olefirelena98@gmail.com>
2023-03-13 23:37:05 -0400

    delete button

--- client/src/components/ui/Carousel/Tile.jsx
+++ client/src/components/ui/Carousel/Tile.jsx
@@ -1,40 +1,68 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {
+  getAllCities,
+  getAllCountries,
+  getAllSights,
+  removeCity,
+  removeCountry,
+  removeSight,
+} from '../../../redux';
 import { Button } from '../Button';
 import './Tile.css';
+
+const REMOVE_DISPATCHERS = {
+  country: removeCountry,
+  city: removeCity,
+  sight: removeSight,
+};
+
+const GET_ALL_DISPATCHERS = {
+  country: getAllCountries,
+  city: getAllCities,
+  sight: getAllSights,
+};
 
 export const Tile = ({ tile, route }) => {
   const path = `${route}/${tile._id}`;
 
   const [isHovered, setIsHovered] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleDeleteButtonClick = async (e) => {
+    await dispatch(REMOVE_DISPATCHERS[route](tile._id));
+    dispatch(GET_ALL_DISPATCHERS[route]());
+  };
 
   return (
-    <Link to={path}>
-      <div
-        className="tile-container"
-        onMouseOver={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
+    <div
+      className="tile-container"
+      onMouseOver={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Link to={path}>
         <div className="tile">
           <img src={tile.img} alt="" className="img-tile" />
           <h3 className="h3-tile">{tile.name}</h3>
         </div>
-        {isHovered && (
-          <div className="tile-button-container">
-            <Link to={`edit-${path}`}>
-              <Button
-                title={<span class="material-symbols-outlined">edit</span>}
-                secondary
-              />
-            </Link>
-
+      </Link>
+      {isHovered && (
+        <div className="tile-button-container">
+          <Link to={`edit-${path}`}>
             <Button
-              title={<span class="material-symbols-outlined">delete</span>}
+              title={<span className="material-symbols-outlined">edit</span>}
               secondary
             />
-          </div>
-        )}
-      </div>
-    </Link>
+          </Link>
+
+          <Button
+            title={<span className="material-symbols-outlined">delete</span>}
+            onClick={handleDeleteButtonClick}
+            secondary
+          />
+        </div>
+      )}
+    </div>
   );
 };
