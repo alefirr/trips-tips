439a598445cfdcb1fd29557f2f65ab630002fcfc
Olena Olefir <olefirelena98@gmail.com>
2023-05-12 16:26:17 -0700

    postgre config migration

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
@@ -16,9 +15,6 @@ dotenv.config();
 
 // Constants
 const PORT = process.env.PORT || 3001;
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_NAME = process.env.DB_NAME;
 
 // Middlewares
 app.use(cors());
@@ -26,6 +22,7 @@ app.use(fileUpload());
 app.use(express.json());
 app.use(express.static('uploads'));
 
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/countries', countryRoutes);
 app.use('/api/cities', cityRoutes);
@@ -35,12 +32,7 @@ app.use('/api/continents', continentRoutes);
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.xncg2h5.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
-    );
-    app.listen(PORT, () =>
-      console.log(`Connected to database, starting server on port ${PORT}`)
-    );
+    app.listen(PORT, () => console.log(`Starting server on port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
-
-const UserSchema = Schema({
-  username: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-});
-
-export default mongoose.model('User', UserSchema);
