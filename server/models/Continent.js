5edb5c5184f39eb8ecb3ae9f9a10669c7f425ed4
Olena Olefir <olefirelena98@gmail.com>
2023-05-13 22:05:29 -0700

    removed models

--- server/models/Continent.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
-
-const ContinentSchema = Schema({
-  name: { type: String, required: true },
-  text: { type: String },
-  img: { type: String, default: '' },
-});
-export default mongoose.model('Continent', ContinentSchema);
--- server/models/Type.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
-
-const TypeSchema = Schema({
-  name: { type: String, required: true },
-});
-export default mongoose.model('Type', TypeSchema);
