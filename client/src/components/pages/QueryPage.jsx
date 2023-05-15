fde7d622cdae7ab64f88beb4adcf221fdc570f88
antliann <antonliann@gmail.com>
2023-05-14 22:59:26 -0700

    imports fix

--- client/src/components/pages/QueryPage.jsx
+++ client/src/components/pages/QueryPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Button } from '../ui';
 import axios from 'axios';
+import { Button } from '../ui';
 
 const REQUESTS = [
   {
